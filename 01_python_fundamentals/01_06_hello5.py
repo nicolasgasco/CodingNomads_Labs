3549f9daa750f5f6b143ab59da564bb663719e5b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2020-10-21 12:14:53 +0200

    Modified two exercises

--- 01_python_fundamentals/01_06_hello5.py
+++ 01_python_fundamentals/01_06_hello5.py
@@ -6,3 +6,5 @@ Write the necessary code to display "Hello World!" 5 times.
 '''
 for x in range(0, 5):
     print("Hello, World!")
+
+print("Hello, World! " * 5)
--- 02_basic_datatypes/2_strings/02_07_replace.py
+++ 02_basic_datatypes/2_strings/02_07_replace.py
@@ -8,3 +8,12 @@ Result: #ore python progra##ing please
 
 '''
 
+# get user input (sentence)
+sentence = input("Please enter a sentence: ")
+
+# get user input (symbol)
+symbol = input("Please enter a symbol: ")
+
+# get first letter of sentence
+print(sentence, symbol)
+# replace all occurrences of first letter with symbol
