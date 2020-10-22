264c9b52a1b31d8061b63cccba3ecf19030d7b39
Nicolas Gasco <nicolas.gasco92@gmail.com>
2020-10-22 16:52:12 +0200

    Refined an exercise

--- 02_basic_datatypes/2_strings/02_11_slicing.py
+++ 02_basic_datatypes/2_strings/02_11_slicing.py
@@ -11,8 +11,7 @@ For example: ryan -> yanray, caden -> adencay
 # Take user name
 user_name = input("Please enter your user name: ")
 # Move first letter to end
-first_letter = user_name[0]
-pig_name = user_name[1:] + first_letter + "ay"
+pig_name = user_name[1:] + user_name[0] + "ay"
 
 # Print out name in pig latin
 print("This is your name in pig Latin:", pig_name.capitalize())
