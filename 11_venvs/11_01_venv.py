293ca5674e7186f4e4e0358b2b1c00ed1c8de712
Nicolas Gasco <nicolas.gasco92@gmail.com>
2020-11-09 20:27:16 +0100

    Completed exercise on venv

--- 11_venvs/11_01_venv.py
+++ 11_venvs/11_01_venv.py
@@ -10,3 +10,33 @@ In your CodingNomads folder create a new folder. Inside of that folder:
 7. Create a new virtual environment and install the packages from the requirements.txt file.
 
 '''
+
+# dir is venv_test
+
+# 1
+# python3 -m venv my_first_venv
+
+# 2
+# source bin/activate
+
+# 3
+# pip install django
+# pip install flask
+# pip install numpy
+# pip install psutil
+
+
+#4.
+pip freeze --local > requirements.txt
+cat requirements.txt
+
+# 5.
+# deactivate
+
+# 6.
+# rm -rf my_first_venv/
+
+# 7.
+# source env/bin/activate
+# pip install -r requirements.txt
+
