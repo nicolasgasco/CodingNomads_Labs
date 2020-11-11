52dae1a4279cb9d6676619d8ab419379663a3e7a
Nicolas Gasco <nicolas.gasco92@gmail.com>
2020-11-11 18:43:59 +0100

    Completed exercises on variable arguments

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def token_color(*args):
+    for color in args:
+        print(f"Please pick a {color} token")
+
+token_color("red", "blue", "yellow", "white", "black", "brown", "purple", "green")
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def my_dict(**kwargs):
+    for k, v in kwargs.items():
+        print(f"{k} = {v}")
+
+my_dict(car1="Ferrari", car2="Maserati", car3="Lamborghini", car4="Bugatti")
