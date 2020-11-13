5b0a4e92ac5a87acf5c1aa2d6c325a96ccb22458
Nicolas Gasco <nicolas.gasco92@gmail.com>
2020-11-13 16:26:53 +0100

    Completed first exercise on decorators

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -1,4 +1,20 @@
+
 '''
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+
+def p_html(inititial_func):
+    def wrapper_func(input):
+        print(f"<p>{input}</p>")
+    return wrapper_func
+
+@p_html
+def simple_print(text):
+    print(text)
+
+simple_print("This is the first paragraph")
+
+
+
+
